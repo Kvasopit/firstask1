1f82c50c9bdf704827438fa6d76749237fbd1d3a
Kvasopit <shdwfndx@google.com>
2025-03-11 16:22:08 +0700

    add variants

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
-let product = "Socks";
-
 let app = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
     product: "Socks",
     image: "./assets/vmSocks-green-onWhite.jpg",
-    link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks"
+    altText: "A pair of socks",
+    inStock: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green"
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue"
+      }
+    ],
+    sizes: ["S", "M", "L", "XL", "XXL", "XXXL"] // Добавляем массив размеров
   }
-})
+});
